bee72a1569559b916b009c3289d85b93dd2c3579
Ryan Berdeen <ryan@ryanberdeen.com>
2013-11-24 15:28:39 -0500

    make command reusable

--- command.js
+++ command.js
@@ -1,22 +1,49 @@
 #!/usr/bin/env node
 var oog = require('./oog');
-var args = process.argv.slice(2);
-var goog = oog(args.shift());
-var main = null;
-while (args.length > 0) {
-  var arg = args.shift();
-  if (arg === '-m') {
-    main = args.shift();
-    break;
+
+function parseArgs() {
+  var args = process.argv.slice(2);
+  var base = args.shift();
+  var load = [];
+  var run = null;
+
+  while (args.length > 0) {
+    var arg = args.shift();
+    if (arg === '-m') {
+      run = {main: args.shift(), args: args};
+      break;
+    }
+    else {
+      load.push(arg);
+    }
   }
-  else {
-    goog.load(arg);
+  return {base: base, load: load, run: run};
+}
+
+function init(opts) {
+  var goog = oog(opts.base);
+  for (var i = 0; i < opts.load.length; i++) {
+    goog.load(opts.load[i]);
+  }
+  return goog;
+}
+
+function run(opts, goog) {
+  if (opts.run) {
+    var parts = opts.run.main.split('.');
+    parts.pop();
+    goog.require(parts.join('.'));
+    goog.require(opts.run.main).apply(null, opts.run.args);
   }
 }
 
-if (main) {
-  var parts = main.split('.');
-  parts.pop();
-  goog.require(parts.join('.'));
-  goog.require(main).apply(null, args);
+if (require.main === module) {
+  var opts = parseArgs();
+  var goog = init(opts);
+  run(opts, goog);
+}
+else {
+  exports.parseArgs = parseArgs;
+  exports.init = init;
+  exports.run = run;
 }
